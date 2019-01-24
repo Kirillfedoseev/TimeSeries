9693443cc2f41ae0f350244554b5bcc85084b788
kirillfedoseev <kirill1998fed@gmail.com>
2019-01-24 20:55:08 +0300

    Remade logic of median; made with linq; Looks more beatiful;

--- TimeSeries/Analyzer.cs
+++ TimeSeries/Analyzer.cs
@@ -8,8 +8,8 @@ namespace TimeSeries
 {
     public class Analyzer
     {
-        private Vector<double>[] _train;
-        private Vector<double>[] _test;
+        private readonly Vector<double>[] _train;
+        private readonly Vector<double>[] _test;
         private readonly int _paramsLength;
 
         public Analyzer(Vector<double>[] train, Vector<double>[] test)
@@ -27,73 +27,49 @@ namespace TimeSeries
         public Matrix<double> Analyze()
         {
             Matrix<double> d = CalculateD();
-
             Matrix<double> invB = CalculateB(d).PseudoInverse();
             Matrix<double> c = CalculateC(d);
             Matrix<double> w = (invB * c).NormalizeColumns(1.0d);
-            
+          
             return d * w;
         }
 
 
         private Matrix<double> CalculateD()
         {
-            if(_train.Length == 0) return null;
+            int len = _train.Length;
+            if (len == 0) return null;
 
-            int[] min = new int[_paramsLength];
-            int[] mid = new int[_paramsLength];
-            int[] max = new int[_paramsLength];
-
+            List<Vector<double>> dSeries = new List<Vector<double>>(3 * _paramsLength);
 
             for (int i = 0; i < _paramsLength; i++)
             {
-                //min[i] = 0;
-                //mid[i] = 0;
-                //max[i] = 0;
+                var res = _train.OrderBy(n => n[i]).ToList();
 
-                for (int j = 1; j < _train.Length; j++)
-                {
-                    if (_train[min[i]][i] > _train[j][i]) min[i] = j;
-                    if (_train[max[i]][i] < _train[j][i]) max[i] = j;
-                }
+                dSeries.Add(res[0]); //min
+                dSeries.Add(res[(len - 1) / 2]); //mid
+                dSeries.Add(res[len - 1]); //max
+            }      
 
-                double median = (_train[min[i]][i] + _train[max[i]][i]) / 2; //arithmetic median
-
-                for (int j = 0; j < _train.Length; j++)
-                {
-                    if (Math.Abs(median - _train[j][i]) < Math.Abs(median - _train[mid[i]][i]))
-                        mid[i] = j; //index of real median
-                }
-            }
-
-            List<int> indexes = new List<int>(_train.Length);
-            for (int i = 0; i < _paramsLength; i++)
-            {
-                if (!indexes.Contains(min[i])) indexes.Add(min[i]);
-                if (!indexes.Contains(mid[i])) indexes.Add(mid[i]);
-                if (!indexes.Contains(max[i])) indexes.Add(max[i]);
-            }
-            Vector<double>[] dSeries = new Vector<double>[indexes.Count];
-            for (int i = 0; i < dSeries.Length; i++)
-                dSeries[i] = _train[indexes[i]];
-
-            return Matrix.Build.DenseOfColumnVectors(dSeries);
+            return Matrix.Build.DenseOfColumnVectors(dSeries.Distinct());
         }
 
         private Matrix<double> CalculateB(Matrix<double> d)
         {
-            double[,] core = new double[d.ColumnCount, d.ColumnCount];
+            Matrix<double> core  = Matrix<double>.Build.Dense(d.ColumnCount, d.ColumnCount);
+
             for (int i = 0; i < d.ColumnCount; i++)
             {
                 for (int j = 0; j < d.ColumnCount; j++)
                     core[i, j] = GetSimilarity(d.Column(i), d.Column(j));
-            }            
-            return Matrix.Build.DenseOfArray(core);
+            } 
+            
+            return core;
         }
 
         private Matrix<double> CalculateC(Matrix<double> d)
         {
-            double[,] cMatrix = new double[d.ColumnCount, _test.Length];
+            Matrix<double> cMatrix = Matrix<double>.Build.Dense(d.ColumnCount, _test.Length);
 
             for (int i = 0; i < d.ColumnCount; i++)
             {
@@ -101,12 +77,11 @@ namespace TimeSeries
                     cMatrix[i, j] = GetSimilarity(d.Column(i), _test[j]);
             }
 
-            return Matrix.Build.DenseOfArray(cMatrix);
+            return cMatrix;
         }
 
         private double GetSimilarity(Vector<double> a, Vector<double> b)
         {
-
             double dist = (a-b).PointwisePower(2).Sum(); //calculate distance between vectors
 
             //sum >=0, due to squares
